5e3d9b4c65f60cc2d3e5955379e4345d8c188956
PocketDx <dairojavierrg@ufps.edu.co>
2025-03-04 00:20:46 -0500

    Training
    Uso del DOM y variables

---
+++ main.js
@@ -0,0 +1,9 @@
+alert('Hi World');
+
+var nombre = "Javier Rodriguez";
+var altura = 168;
+
+var concatenacion = nombre + " " + altura;
+
+var datos = document.getElementById('datos');
+datos.innerHTML = concatenacion;
