d3507a04b4ebe2bee1e6f690058547449b50788e
PocketDx <dairojavierrg@ufps.edu.co>
2025-06-12 11:58:28 -0500

    Updates

---
+++ PokeApi/js/index.js
@@ -0,0 +1,33 @@
+const listaPokemon = document.getElementById("listaPokemon");
+const URLpokemon = "https://pokeapi.co/api/v2/type";
+
+/**
+
+async function fetchPokemon() {
+  try {
+    const response = await fetch(URL);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    data.results.forEach((pokemon) => {
+      const li = document.createElement("li");
+      li.textContent = pokemon.name;
+      listaPokemon.appendChild(li);
+    });
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
+}
+
+*/
+fetch(URLpokemon)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        data.results.forEach(tipo =>{
+            const li = document.createElement("li");
+            li.textContent = tipo.name;
+            listaPokemon.appendChild(li);
+        })
+    });
---
+++ RickMortyTest/page2.js
@@ -0,0 +1,30 @@
+function obtenerPersonajes(done){
+    const result = fetch("https://rickandmortyapi.com/api/character");
+    result
+        .then(response => response.json())
+        .then(data => {
+            done(data)
+        });
+}
+
+obtenerPersonajes(data => {
+    // console.log(data);
+
+    data.results.forEach(personaje => {
+        
+        const article = document.createRange().createContextualFragment(/*html*/`
+            <article>
+
+                <div class="image-container">
+                    <img src="${personaje.image}" alt="personaje">
+                </div>
+
+                <h2>${personaje.name}</h2>
+                <span>${personaje.status}</span>
+
+            </article>
+            `);
+        const main = document.querySelector("main");
+        main.append(article);
+    });
+});
