9aae79f5ec9f9789d30a140dadadfd8b39043c8d
PocketDx <dairojavierrg@ufps.edu.co>
2025-06-13 01:21:07 -0500

    Update
    Mejoras importantes

--- PokeApi/js/index.js
+++ PokeApi/js/index.js
@@ -1,4 +1,8 @@
-const URLpokemon = "https://pokeapi.co/api/v2/type";
+const botones = document.getElementById("botones");
+const listaPokemon = document.getElementById("listaPokemon");
+
+const URLtipo = "https://pokeapi.co/api/v2/type";
+const URLpokemon = "https://pokeapi.co/api/v2/pokemon/";
 
 /**
 
@@ -20,7 +24,8 @@ async function fetchPokemon() {
 }
 
 */
-fetch(URLpokemon)
+
+fetch(URLtipo)
     .then((response) => response.json())
     .then((data) => {
         console.log(data);
@@ -29,8 +34,48 @@ fetch(URLpokemon)
         });
     });
 
+for (let i = 1; i <= 120; i++) {
+    fetch(URLpokemon + i)
+    .then(response => response.json())
+    .then(pokemon => mostrarPokemon(pokemon))
+}
+
 function crearFiltros(tipo){
     const btn = document.getElementById("filtro");
     btn.addEventListener("click", () => {
+      const btnFiltro = document.createElement("button");
+      btnFiltro.id = tipo;
+      btnFiltro.className = "btn";
+      btnFiltro.textContent = tipo;
+      botones.appendChild(btnFiltro);
+      btn.disabled = true;
     });
+}
+
+function mostrarPokemon(pokemon) {
+  const tipos = pokemon.types.map(tipo => `<p class="tipo">${tipo.type.name}</p>`).join('');
+  const div = document.createElement("div");
+  div.classList.add("pokemon");
+  div.innerHTML = `
+    <div class="pokemon-imagen">
+      <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+    </div>
+    <div class="pokemon-info">
+      <div class="nombre-contenedor">
+        <p class="poke-id">${pokemon.id}</p>
+        <h2 class="poke-nombre">${pokemon.name}</h2>
+      </div>
+      <div class="poke-tipos">
+        ${tipos}
+      </div>
+      <div class="poke-stats">
+        <p class="stat">${pokemon.height}m</p>
+        <p class="stat">${pokemon.weight} kg</p>
+      </div>
+    </div>`
+    listaPokemon.appendChild(div);
+}
+
+function filtrarPokemon() {
+  const btn = document.getElementById("filtro");
 }
